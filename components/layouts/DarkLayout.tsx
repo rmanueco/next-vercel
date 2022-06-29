324d38c66d464c5e867469f01023c153ed9dd718
Rodrigo <manueco.rodrigo@gmail.com>
2022-06-29 12:37:54 +0200

    Pre-deployment vercel

---
+++ components/layouts/DarkLayout.tsx
@@ -0,0 +1,17 @@
+import { FC, PropsWithChildren } from 'react'
+
+type Props = {};
+export const DarkLayout: FC<PropsWithChildren<Props>> = ({ children }) => {
+  return (
+    <div style={{
+        backgroundColor: 'rgba(0,0,0,0.3)',
+        borderRadius: '5px',
+        padding: '10px',
+    }}>
+        <h3>Dark-Layout</h3>
+        <div>
+            { children } 
+        </div>
+    </div>
+  );
+};
---
+++ pages/_app.tsx
@@ -0,0 +1,31 @@
+import '../styles/globals.css'
+
+import { NextPage } from 'next'
+import { ReactElement, ReactNode } from 'react';
+import { AppProps } from 'next/app';
+
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+
+
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+
+  const getLayout = Component.getLayout || ((page) => page );
+
+  // return (
+  //   <>
+  //     <Component {...pageProps} />
+  //   </>
+  // )
+  return getLayout( <Component {...pageProps} /> )
+}
+
+export default MyApp
